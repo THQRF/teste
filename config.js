58e85492ecdd95305d57c50a3ebf41db1b7d6112
THQRF <tqrfilho@gmail.com>
2025-12-31 02:14:37 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+﻿// CONFIGURAÇÕES DO SERVIDOR PHOTON
+const SERVER_CONFIG = {
+    APP_ID: "927bad9b-6de1-4cea-b5aa-510d5c2e5146", // Substitua pelo seu ID
+    APP_VERSION: "1.0",
+    REGION: "us", // 'us' para EUA, 'sa' para América do Sul (Brasil)
+};
